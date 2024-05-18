cec402e875a3c34e5ddecfa48da57483d321b0c2
DerHumbert <jhsgmx@gmail.com>
2024-05-18 14:02:23 -0600

    Adding function to automatically download CSV file with melate results

--- get_data.py
+++ get_data.py
@@ -1,22 +1,28 @@
 import requests
 import bs4
+import re 
 
 
 def filter_links(tag):
 	if isinstance(tag, str):
 		return tag.endswith('csv')
 
-def get_links(tags_list):
-	return [WEB_ROOT + tag.attrs['href'] for tag in tags_list]
+def get_links(links):
+	urls = []
+	for link in links:
+		urls.append(link.get('href'))
+	return urls
 
-def download_file(link, directory):
-	file = requests.get(link).content
-	name = file_link.split('/')[-1]
-	write_path = directory + name
+
+
+def download_file(link):
+	file = requests.get(link, verify=False)
+	print(file.text)
+	write_path = 'data/Melate.csv'
 
 	print("Writing file: ", write_path)
-	with open(write_path, 'wb') as fp:
-		fp.write(file)
+	with open(write_path, 'w') as fp:
+		fp.write(file.text)
 
 if __name__ == "__main__":
     WEB_ROOT = 'https://www.loterianacional.gob.mx'
@@ -25,7 +31,15 @@ if __name__ == "__main__":
     r = requests.get('https://www.loterianacional.gob.mx/Melate/Resultados', verify=False)
 
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
-    results = soup.select("p#melate > a")
-    links = get_links(results)
+    links = soup.find_all('a', href=True)
+    urls = get_links(links)
 
-    print(links)
+    q = re.compile(".*/Home/Historicos")
+    hist = list(filter(q.match,urls))
+    print(hist)
+
+    url = hist[0][5:]
+    url = WEB_ROOT + url 
+    print(url)
+    
+    download_file(url)
